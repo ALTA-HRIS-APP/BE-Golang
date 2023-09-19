b230b9dfab6e647bcd86299492703c16e5b1541f
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-09-19 08:05:13 +0700

    authorization: create target

--- features/target/service/logic.go
+++ features/target/service/logic.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"be_golang/klp3/features/target"
+	usernodejs "be_golang/klp3/features/userNodejs"
 	"errors"
 	"log"
 
@@ -22,18 +23,37 @@ func New(repo target.TargetDataInterface) target.TargetServiceInterface {
 
 // Create implements target.TargetServiceInterface.
 func (s *targetService) Create(input target.TargetEntity) (string, error) {
-	// responseUser, err := usernodejs.GetByIdUser(input.UserIDPembuat)
-	// if err != nil {
-	// 	log.Printf("Error get detail user: %s", err.Error())
-	// 	return "",err
-	// }
-	err := s.validate.Struct(input)
+	userPembuat, err := usernodejs.GetByIdUser(input.UserIDPembuat)
+	if err != nil {
+		log.Printf("Error get detail user: %s", err.Error())
+		return "", err
+	}
+	userPenerima, err := usernodejs.GetByIdUser(input.UserIDPenerima)
+	if err != nil {
+		log.Printf("Error get detail user: %s", err.Error())
+		return "", err
+	}
+	err = s.validate.Struct(input)
 	if err != nil {
 		log.Printf("Error validate: %s", err.Error())
 		return "", errors.New("error validate, konten target, due date required")
 	}
 
-	// if responseUser.Jabatan=="manager"
+	if userPembuat.Jabatan == "c-level" {
+		if userPenerima.Jabatan != "karyawan" && userPenerima.Jabatan != "manager" {
+			return "", errors.New("c-level hanya bisa membuat target untuk karyawan atau manager")
+		}
+	}
+
+	if userPembuat.Jabatan == "manager" {
+		if userPenerima.Jabatan != "karyawan" {
+			return "", errors.New("manager hanya bisa membuat target untuk karyawan")
+		}
+	}
+
+	if userPembuat.Jabatan != "c-level" && userPembuat.Jabatan != "manager" {
+		return "", errors.New("jabatan Anda tidak memiliki izin untuk membuat target")
+	}
 
 	targetID, err := s.targetRepo.Insert(input)
 	if err != nil {
